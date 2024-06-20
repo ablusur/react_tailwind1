cbeb78df7ee55c2c7dd684779f80ac2513f57b96
Ablusur <mdablusur@gmail.com>
2024-06-20 22:14:52 +0600

    tailwindcss add

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+
+  return (
+    <>
+    <div className="bg-[blue] h-[100px] mx-auto py-10 max-w-contianer">
+    <h1 className="text-clr font-bold">hello</h1>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ my-project/tailwind.config.js
@@ -0,0 +1,22 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'osans': ['Open Sans', 'sans-serif'],
+      },
+      maxWidth:{
+        contianer:"1120px"
+      },
+      colors:{
+        clr:"green"
+      }
+    },
+  },
+  plugins: [],
+}
